09893dc4113de1405922400ecb206c672fa1033a
Alexander Medvedev <mega.medved87@gmail.com>
2024-01-05 09:44:21 -0600

    install dependencies for 3d Home page and was added Canvas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+
+{/* <div className='absolute top-28 left-0 right-0 z-10 items-center justify-center flex'>
+  POPUP
+</div> */}
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className='w-full h-screen relative'>
+
+    </section>
   )
 }
 
