f6405d31b22ad03cbc8312ff800adb8099518b0b
Alexander Medvedev <mega.medved87@gmail.com>
2024-01-05 00:03:47 -0600

    All setups for our future 3d Website

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={'Home'} />
+          <Route path="/" element={'About'} />
+          <Route path="/" element={'Projects'} />
+        </Routes>
+      </Router>
+    </main>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
-import { ReactDOM } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>) 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
